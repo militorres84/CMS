07d52d3afe0c98c1d7f3cb1d31106729b4423f92
Zoe Carida <Zowycar@gmail.com>
2023-09-11 09:06:41 -0300

    Resolucion Registro

--- frontend/vite-project/src/pages/register/register.jsx
+++ frontend/vite-project/src/pages/register/register.jsx
@@ -1,97 +1,51 @@
 import React from "react"
-import './css/register.scss'
+import './css/_register.scss'
 
 function Register() {
     return (
-        <body id="body-register">
-            <div className="register-box">
+        <div className="body-register">
+            <div className="container-register">
+
                 <div className="welcome-register">
-                    <h1 className="">¡Bienvenido!</h1>
+                    <h1 className="welcome-register-text"> ¡Bienvenidos! </h1>
                 </div>
 
-                <div className="register-usser-email">
-                    <label for="username" className="label-register-ussername">
-                    </label>
-                    <input
-                        className="input-register-ussername"
-                        id="username"
-                        name="username"
-                        type="text"
-                        placeholder="Nombre de Usuario"
-                        required></input>
+            <label className="label-reg"></label>
+            <input className="user-reg" id="username" type="text" placeholder="Nombre de Usuario" required></input>
+            
+            <label className="label-reg"></label>
+            <input className="user-reg" id="email" type="email" placeholder="Correo Electronico" required></input>
 
-                    <label for="email" className="label-register-email">
-                    </label>
-                    <input
-                        className="input-register-email"
-                        id="email"
-                        name="email"
-                        type="email"
-                        placeholder="Correo Electronico"
-                        required></input>
-                </div>
+            <label className="label-reg"></label>
+            <input className="user-reg" id="password" type="password" placeholder="Contraseña" required></input>
+        
+            <label className="label-reg"></label>
+            <input className="user-reg" id="confirm-password" type="password" placeholder="Confirmar Contraseña" required></input>
 
-                <div className="register-password">
-                    <label for="password" className="label-register-password">
-                    </label>
-                    <input
-                        className="input-register-password"
-                        id="password"
-                        name="password"
-                        type="password"
-                        placeholder="Contraseña"
-                        required></input>
-
-                    <label for="password" className="label-register-passwordconfirmed">
-                    </label>
-                    <input
-                        className="input-register-passwordconfirmed"
-                        id="password"
-                        name="password"
-                        type="password"
-                        placeholder="Confirmar Contraseña"
-                        required></input>
-                </div>
-
-                <div className="checkbox-terms-and-conditions">
-                    <label for="checkbox" className="terms-and-conditions"></label>
-                    <input
-                        checked="checked"
-                        type="radio"
-                        name="radio"
-                        id="terms-and-conditions"
-                        required> Aceptar <a href="#">  terminos y condiciones.</a>
-                    </input>
-                </div>
-
-                <div>
-                    <p className="register-text">Crear</p>
-                    <button className="register-btn"> - </button>
-                </div>
-
-                <div className="social-media">
-                    <p>
-                        O continuar con...
-                    </p>
-
-                    <div>
-                        <button className="google-btn">G</button>
-                        <button className="facebook-btn">F</button>
-                    </div>
-                </div>
-
-                <div className="existing-account">
-                    <p>
-                        <a href="#" className="login-sesion"> ¿Ya tienes una cuenta? </a>
-                    </p>
-                </div>
-
-                <div>
-                    <button className="register-btn-deskopt"> Crear </button>
-                </div>
-
+            <div className="checkbox-terms-and-conditions">
+                <label className="label-checkbox-reg">
+                </label>
+                <input className="checked"
+                        type="checkbox"
+                        id="term-and-conditions"
+                        name="Aceptar terminos y condiciones."
+                        required>
+                </input>
             </div>
-        </body>
+
+            <p className="register-text"> Crear </p>
+            <button className="reg-btn"> - </button> 
+            <button className="reg-deskopt-btn"> Crear </button>
+            <p className="socialmedia-text"> 
+            O continua con...
+            </p>
+            <div className="socialmedia-btn">
+                <button className="google-btn">G</button>
+                <button className="facebook-btn">F</button>  
+            </div> 
+            <p><a href="#" className="login-sesion">¿Ya tienes una cuenta? </a></p>    
+    </div>
+   </div> 
     );
 }
 
