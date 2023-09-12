f0e14ed3372d6dd3a420313502633805d5fad27c
unknown <johndoe@example.com>
2023-09-12 10:53:13 -0300

    fix-styles

--- frontend/vite-project/src/pages/home/quienes.jsx
+++ frontend/vite-project/src/pages/home/quienes.jsx
@@ -3,9 +3,20 @@ import './_quienes.scss';
 
 const Grupo = () => {
 
-    <div>
-        <h2>¿Quienes somos?</h2>
-        <div className='text'>
-            <p>Somos un grupo de desarrolladoras que forman parte del bootcam conjunto entre "Fundacion Empujar" y "Somos Coders"</p></div>
-    </div>
+return (    
+    
+    <div className='section'>
+    <div className='explicacion'>
+<h2>¿Quienes somos?</h2>
+<div className='text'>
+    <p>Somos un grupo de desarrolladoras predispuestas y con ganas de aprender
+        <br/>
+        que forman parte del bootcam conjunto entre "Fundacion Empujar" y "Somos Coders". 
+        <br />
+        hemos creado una pagina CMS donde podras postear tus fotos favoritas de forma sencilla y facil.</p></div>
+        </div>
+</div>
+
+);
+
 }
--- frontend/vite-project/src/pages/home/tuto.jsx
+++ frontend/vite-project/src/pages/home/tuto.jsx
@@ -3,5 +3,9 @@ import './_tuto.scss';
 
 const Tutorial = () => {
 
-    
+    <div>
+        <div className='tittle'>
+            <h2>¿Como crear un posteo?</h2>
+        </div>
+    </div>
 }
