b53df759d0903ca74c7709e698927a77922f6c0a
unknown <johndoe@example.com>
2023-09-12 10:32:46 -0300

    fix-vistas

---
+++ frontend/vite-project/src/pages/home/quienes.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './_quienes.scss';
+
+const Grupo = () => {
+
+    <div>
+        <h2>¿Quienes somos?</h2>
+        <div className='text'>
+            <p>Somos un grupo de desarrolladoras que forman parte del bootcam conjunto entre "Fundacion Empujar" y "Somos Coders"</p></div>
+    </div>
+}
